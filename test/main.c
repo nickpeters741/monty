898e073ddaae13c1e5e14565c8bb4fffde2ddc8f
nickpeters741 <npeters741@gmail.com>
2021-05-10 12:06:36 +0000

    Accepts only 1 command line argument

---
+++ test/main.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+int main(int argc, char *argv[])
+{
+
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("File name: %s\n",argv[1]);
+
+	return (0);
+}
