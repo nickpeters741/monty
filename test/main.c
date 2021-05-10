01460104cba27c22bb1c94b9d524ca8bf27502e0
nickpeters741 <npeters741@gmail.com>
2021-05-10 12:42:47 +0000

    Open the passed file

--- test/main.c
+++ test/main.c
@@ -1,13 +1,21 @@
 #include "monty.h"
+
 int main(int argc, char *argv[])
 {
+	FILE *fp;
 
 	if (argc != 2)
 	{
 		printf("USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	printf("File name: %s\n",argv[1]);
+
+	fp = fopen(argv[1], "r");
+	if (fp == NULL)
+	{
+		printf("Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
 	return (0);
 }
