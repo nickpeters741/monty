34fbc29b044cc2f2d07547f9eaef5e3a6e16a226
nickpeters741 <npeters741@gmail.com>
2021-05-10 13:00:07 +0000

    Read the file 1 line at a time

--- test/main.c
+++ test/main.c
@@ -3,6 +3,8 @@
 int main(int argc, char *argv[])
 {
 	FILE *fp;
+	char * line = NULL;
+	size_t len = 0;
 
 	if (argc != 2)
 	{
@@ -10,12 +12,21 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
+	/* Open the file */
 	fp = fopen(argv[1], "r");
+	/* Confirm the file has opened succesfully */
 	if (fp == NULL)
 	{
 		printf("Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+
+	/* Read the file one line at a time */
+	while (getline(&line, &len, fp) != -1)
+	{
+		printf("%s", line);
+	}
+
 
 	return (0);
 }
