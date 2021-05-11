9b73366669501d889bd067a0598a3c34735cd314
nickpeters741 <npeters741@gmail.com>
2021-05-11 07:27:17 +0000

    Add tokenize function

---
+++ test/tokenize.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+char **tokenize(char *s)
+{
+	char *token;
+	int i = 0;
+	static char *arr[10] = {NULL};
+
+	token = strtok(s, " ");
+	while (token != NULL)
+	{
+		arr[i++] = token;
+		token = strtok(NULL, " ");
+	}
+
+	return (arr);
+}
