9592387eb4de02b6c5cdc0f2735219f9c7aeff93
nickpeters741 <npeters741@gmail.com>
2021-05-11 07:27:02 +0000

    Add prototype for tokenize function

--- test/monty.h
+++ test/monty.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -33,5 +34,9 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/* Function Prototypes */
+char *remove_space(char *s);
+char **tokenize(char *s);
 
 #endif /* MONTY_H */
